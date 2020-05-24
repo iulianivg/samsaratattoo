968193c2a69b1ba08e4ba03bd51f6bea45ffb339
unknown <eqnope@gmail.com>
2020-05-24 20:05:28 +0100

    Updates

---
+++ precache-manifest.a521a89becbe73db985b01af898464b2.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba06200f2d730d984c4b1830a1fefdad",
+    "url": "/samsaratattoo/index.html"
+  },
+  {
+    "revision": "5364e935af0afeace8b7",
+    "url": "/samsaratattoo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "fc8122c681b2d6b51f6b",
+    "url": "/samsaratattoo/static/css/main.a758a826.chunk.css"
+  },
+  {
+    "revision": "5364e935af0afeace8b7",
+    "url": "/samsaratattoo/static/js/2.c8188859.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/samsaratattoo/static/js/2.c8188859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc8122c681b2d6b51f6b",
+    "url": "/samsaratattoo/static/js/main.07ac8c09.chunk.js"
+  },
+  {
+    "revision": "b03150fe84c1701ae8bb",
+    "url": "/samsaratattoo/static/js/runtime-main.ca55da4b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/samsaratattoo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "a3b82a57a752cc9f16e4e3377e0283b0",
+    "url": "/samsaratattoo/static/media/logo.a3b82a57.jpg"
+  },
+  {
+    "revision": "95585aa68b3ed31000e4f5fae6fc3ff4",
+    "url": "/samsaratattoo/static/media/tattoo2.95585aa6.png"
+  },
+  {
+    "revision": "f02f2ccf746d0b228df8aabf416a8167",
+    "url": "/samsaratattoo/static/media/work1.f02f2ccf.jpg"
+  },
+  {
+    "revision": "51800b6e93f98d9f6ec937d0bb377ae4",
+    "url": "/samsaratattoo/static/media/work2.51800b6e.jpg"
+  },
+  {
+    "revision": "cc9afa9c22d76c38c8ada9c6f76196ff",
+    "url": "/samsaratattoo/static/media/work3.cc9afa9c.jpg"
+  },
+  {
+    "revision": "e9f3b231b5c14d29345bc13b5a3df769",
+    "url": "/samsaratattoo/static/media/work4.e9f3b231.jpg"
+  },
+  {
+    "revision": "9464e0a984108815bdcbed7b182303ff",
+    "url": "/samsaratattoo/static/media/work5.9464e0a9.jpg"
+  },
+  {
+    "revision": "403dc787e876bcca624f57b6a4874828",
+    "url": "/samsaratattoo/static/media/work6.403dc787.jpg"
+  }
+]);
